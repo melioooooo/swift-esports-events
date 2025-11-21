0e1cd4950c7090416c32da306db661be2fc28233
Xavier ROGNON <xavierrognon@Xaviers-MacBook-Air.local>
2025-11-22 00:35:01 +0900

    fixing deploy errors

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// Configuration pour le déploiement sur GitHub Pages (site de projet)
 export default defineConfig({
   plugins: [react()],
-  base: '/swift-esports-events/',
+  base: "/",
 });
+
